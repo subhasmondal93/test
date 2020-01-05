9fe61dc5908918b5cb7cb85ff0c8256d6f1385d4
Subhas Mondal <subhas.mondal@ah.nl>
2020-01-05 15:53:59 +0530

    Initial commit

---
+++ src/android/MathCalculator.java
@@ -0,0 +1,53 @@
+package cordova-plugin-mathcalculator;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class MathCalculator extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException 
+    {
+        
+        if(action.equals("add"))
+        {
+            this.add(args, CallbackContext);
+            return true;
+            
+        }
+        return false;
+    }
+
+    
+
+    private void add(JSONArray args, CallbackContext callback)
+    {
+        if(args != null)
+        {
+
+            try{
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+
+                callback.success("" + (p1+p2));
+
+
+
+            }catch(Exception ex){
+                callback.error("Somthing went wrong" +ex);
+            }
+        }
+        else
+        {
+            callback.error("Please do not pass null");
+        }
+
+    }
+}
---
+++ www/MathCalculator.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'coolMethod', [arg0]);
+};
+
+module.exports.add = function (arg0, success, error)
+{
+    exec(success, error, 'MathCalculator' , 'add' , [arg0]);
+}
